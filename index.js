6a6568a79ce0a96352487c45b1c307d99acf9aca
Nguyen Quoc Trung <qtrung811203@gmail.com>
2025-03-07 09:36:11 +0700

    feat: :sparkles: First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// LOAD COMPONENT TO INDEX.HTML
+async function loadComponent(id, file) {
+  const res = await fetch(file);
+  const text = await res.text();
+  document.getElementById(id).innerHTML = text;
+}
+
+// If want to import any file, please import here -.-
+loadComponent("main", "src/components/main/main.html");
+
+//----//
