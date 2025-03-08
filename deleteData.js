3b8d78e8529632f7ef2723653ae7c2b5f1f7674c
vipprotk11abc <bangbangtest123@gmail.com>
2025-03-08 20:19:58 +0700

    AQG-16 delete quiz, questions by quizId on IndexedDB

---
+++ deleteData.js
@@ -0,0 +1,38 @@
+const DB_NAME = "QuizDatabase";
+const DB_VERSION = 1;
+function deleteQuizWithQuestions(quizzId) {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+    request.onsuccess = function (event) {
+        const db = event.target.result;
+        const transaction = db.transaction(["Quizz", "Questions"], "readwrite");
+
+        const quizzStore = transaction.objectStore("Quizz");
+        const questionStore = transaction.objectStore("Questions");
+
+        // Xóa quizz trước
+        quizzStore.delete(quizzId);
+
+        // Xóa tất cả question có quizzId tương ứng
+        const index = questionStore.index("quizzId"); 
+        const request = index.openCursor(IDBKeyRange.only(quizzId));
+
+        request.onsuccess = function (event) {
+            const cursor = event.target.result;
+            if (cursor) {
+                questionStore.delete(cursor.primaryKey);
+                cursor.continue(); // Tiếp tục xóa các question tiếp theo
+            }
+        };
+
+        transaction.oncomplete = function () {
+            console.log(`Deleted quiz ${quizzId} and related questions.`);
+        };
+
+        transaction.onerror = function () {
+            console.error("Error deleting quiz and questions.");
+        };
+    };
+}
+
+export default deleteQuizWithQuestions;
